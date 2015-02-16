5280ba05d8129f1d7953ecc624d179a65a8724ed
Vishnuprabhu <vishnuprabhu.g@gmail.com>
2015-02-16 18:06:32 +0530

    copy of booking and initial commit as it in booking

---
+++ src/java/Domain/Journey.java
@@ -0,0 +1,10 @@
+package Domain;
+
+import java.sql.Date;
+
+public class Journey {
+
+    public long Journeyid;
+    public long trainId;
+    public Date date;
+}
---
+++ src/java/Domain/UnderPassenger.java
@@ -0,0 +1,9 @@
+package Domain;
+
+public class UnderPassenger {
+
+    public long pnr;
+    public String name;
+    public int age;
+    public int gender;
+}
